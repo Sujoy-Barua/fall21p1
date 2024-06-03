96eb064b70cc4ea8e8c359d8c0b31669dafd5d00
Sujoy B <70602535+Sujoy-Barua@users.noreply.github.com>
2024-06-03 17:25:53 -0400

    Add files via upload

--- criticalLoad.m
+++ criticalLoad.m
@@ -1,13 +1,15 @@
+% Sujoy Barua (sujoysb)
+% Lauren Berry (lkberry)
+% Section 020, Sep 13th, 2021.
+
 function [P_cr] = criticalLoad(E, I, K, L)
-%calcCriticalLoad computes Euler's critical load for a column with 
-%  structural parameters: E, I, K, and L.
-%     E: Modulus of elasticity of the column material
-%     I: Minimum moment of inertia across cross section of the column
-%     K: Column effective length factor 
-%     L: Length of column
-%      
-%     P_cr: Euler's critical load of column  
+    %   criticalLoad computes Euler’s critical load for a column with 
+    %    structural parameters: E, I, K, and L.
+    %     E: Modulus of elasticity of the column material
+    %     I: Minimum moment of inertia across cross section of the column
+    %     K: Column effective length factor
+    %     L: Length of column 
+    %     P_cr: Euler’s critical load of column
 
-    % Write your code here!
-
+ P_cr = ((pi^2) .* E .* I) ./ ((K .* L) .^2);
 end
